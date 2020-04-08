7d31aad71fa593b976d62532884f74a87d9c9418
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-08 17:01:07 -0400

    [JT] Splash screen added

---
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0775696_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

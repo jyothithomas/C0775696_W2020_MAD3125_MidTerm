d4ed5723a4fc523a8b16cfccfd3cc6ac02c22bd8
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-08 17:21:54 -0400

    [JT] Splash screen Timeout added

--- app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/SplashActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/SplashActivity.java
@@ -1,14 +1,26 @@
 package com.example.c0775696_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },SPLASH_TIME_OUT);
     }
 }

ba682a96ec66625ee652fa3cf9665a9ff5bf0ea7
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-09 09:10:15 -0400

    [JT] added radio button selection

--- app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/DataDisplayActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/DataDisplayActivity.java
@@ -2,13 +2,44 @@ package com.example.c0775696_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.c0775696_w2020_mad3125_midterm.CRACustomer;
 
 public class DataDisplayActivity extends AppCompatActivity {
+    CRACustomer customer;
+    private TextView lblSin,lblFullName,lblBirthDate,lblAge,lblGender;
+    private TextView lblTaxFilingDate,lblGrossIncome,lblRRSPContributed,lblRRSPCarryFwd;
+    private TextView lblFederalTax,lblProvincialTax,lblEI,lblCpp,lblTaxableIncome,lblTaxPayed;
+
+    double cpp = 0, ei = 0;
+    double rrspContribution = 0, rrspCarryFwd = 0;
+    double taxableIncome, federalTax, provincialTax, totalTaxPaid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_display);
+        lblSin = findViewById(R.id.lblSinNumber);
+        lblFullName = findViewById(R.id.lblFullName);
+        lblBirthDate = findViewById(R.id.lblBirthDate);
+        lblAge = findViewById(R.id.lblAge);
+        lblGender = findViewById(R.id.lblGender);
+        lblTaxFilingDate = findViewById(R.id.txtTaxFillDate);
+        lblGrossIncome = findViewById(R.id.lblGrossIncome);
+        lblRRSPContributed = findViewById(R.id.lblRrsContributed);
+        lblRRSPCarryFwd = findViewById(R.id.lblCarryRrs);
+        lblFederalTax = findViewById(R.id.lblFederalTax);
+        lblProvincialTax = findViewById(R.id.lblProvincialTax);
+        lblEI = findViewById(R.id.lblEi);
+        lblCpp = findViewById(R.id.lblCpp);
+        lblTaxableIncome = findViewById(R.id.lblTotalTaxIncome);
+        lblTaxPayed = findViewById(R.id.lblTaxPayed);
+
+        Intent mIntent = getIntent();
+        CRACustomer customer = mIntent.getParcelableExtra("CRACustomer");
+
     }
 }

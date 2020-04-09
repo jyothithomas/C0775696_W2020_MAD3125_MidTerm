c635d34e3b314f6a094d2d66a37579297966722d
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-08 22:54:13 -0400

    [JT] added Model Class

---
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/CRACustomer.java
@@ -0,0 +1,19 @@
+package com.example.c0775696_w2020_mad3125_midterm;
+
+import java.util.Date;
+
+public class CRACustomer {
+    private String sinNumber;
+    private String firstName;
+    private String  lastName;
+    private String fullName;
+    private  String gender;
+    private Date dateOfBirth,filingTaxDate;
+    private  double fedTax, provTax;
+    private  double rrspCarryForward;
+    private  double grossIncome;
+    private double rrspContribution;
+    private double totalEI;
+    private double totalTaxableAmount;
+    private double totalTaxPaid;
+}

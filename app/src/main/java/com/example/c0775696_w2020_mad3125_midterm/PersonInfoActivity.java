8cf7629f1feb8c33daf9eeacb64dc6a89432ecf0
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-08 22:03:14 -0400

    [JT]button click to new added

--- app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/PersonInfoActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/PersonInfoActivity.java
@@ -2,13 +2,27 @@ package com.example.c0775696_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonInfoActivity extends AppCompatActivity {
 
+    private Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(PersonInfoActivity.this, DataDisplayActivity.class);
+                startActivity(mIntent);
+            }
+        });
+
     }
+
 }

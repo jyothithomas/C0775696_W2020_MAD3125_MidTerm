e1a645afe8488b1859d3e55f3f0bce1e8ec6bb27
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-09 01:28:56 -0400

    [JT] added Date Picker

--- app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/CRACustomer.java
@@ -1,29 +1,213 @@
 package com.example.c0775696_w2020_mad3125_midterm;
 
-import java.util.Date;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class CRACustomer {
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CRACustomer implements Parcelable {
     private String sinNumber;
     private String firstName;
-    private String  lastName;
+    private String lastName;
     private String fullName;
-    private  String personGender;
-    private Date dateOfBirth,filingTaxDate;
-    private  double fedTax, provTax;
-    private  double rrspCarryForward;
-    private  double grossIncome;
+    private String personGender;
+    private Date dateOfBirth, filingTaxDate;
+    private double fedTax, provTax;
+    private double rrspCarryForward;
+    private double grossIncome;
     private double rrspContribution;
     private double totalEI;
     private double totalTaxableAmount;
     private double totalTaxPaid;
 
-    public CRACustomer(String sinNumber,String firstName,String lastName,String personGender,double grossIncome, double rrspContribution)
-    {
+
+
+    public CRACustomer(String sinNumber, String firstName, String lastName, String personGender, double grossIncome, double rrspContribution) {
         this.sinNumber = sinNumber;
         this.firstName = firstName;
         this.personGender = personGender;
         this.grossIncome = grossIncome;
         this.rrspContribution = rrspContribution;
 
+    }
+
+    public String getSinNumber() {
+        return sinNumber;
+    }
+
+    public void setSinNumber(String sinNumber) {
+        this.sinNumber = sinNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFull_name() {
+        return lastName.toUpperCase() + ", " +
+                firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+    }
+
+    public void setFull_name(String full_name) {
+        this.fullName = full_name;
+    }
+
+    public String getPersonGender() {
+        return personGender;
+    }
+
+    public void setPersonGender(String personGender) {
+        this.personGender = personGender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getFilingTaxDate() {
+        return filingTaxDate;
+    }
+
+    public void setFilingTaxDate(Date filingTaxDate) {
+        this.filingTaxDate = filingTaxDate;
+    }
+
+    public double getFedTax() {
+        return fedTax;
+    }
+
+    public void setFedTax(double fedTax) {
+        this.fedTax = fedTax;
+    }
+
+    public double getProvTax() {
+        return provTax;
+    }
+
+    public void setProvTax(double provTax) {
+        this.provTax = provTax;
+    }
+
+    public double getRrspCarryForward() {
+        return rrspCarryForward;
+    }
+
+    public void setRrspCarryForward(double rrspCarryForward) {
+        this.rrspCarryForward = rrspCarryForward;
+    }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public double getRrspContribution() {
+        return rrspContribution;
+    }
+
+    public void setRrspContribution(double rrspContribution) {
+        this.rrspContribution = rrspContribution;
+    }
+
+    public double getTotalEI() {
+        return totalEI;
+    }
+
+    public void setTotalEI(double totalEI) {
+        this.totalEI = totalEI;
+    }
+
+    public double getTotalTaxableAmount() {
+        return totalTaxableAmount;
+    }
+
+    public void setTotalTaxableAmount(double totalTaxableAmount) {
+        this.totalTaxableAmount = totalTaxableAmount;
+    }
+
+    public double getTotalTaxPaid() {
+        return totalTaxPaid;
+    }
+
+    public void setTotalTaxPaid(double totalTaxPaid) {
+        this.totalTaxPaid = totalTaxPaid;
+    }
+
+    protected CRACustomer(Parcel parcel) {
+        sinNumber = parcel.readString();
+        firstName = parcel.readString();
+        lastName = parcel.readString();
+        fullName = parcel.readString();
+        personGender = parcel.readString();
+        provTax = parcel.readDouble();
+        fedTax = parcel.readDouble();
+        rrspCarryForward = parcel.readDouble();
+        grossIncome = parcel.readDouble();
+        rrspContribution = parcel.readDouble();
+        totalEI = parcel.readDouble();
+        totalTaxableAmount = parcel.readDouble();
+        totalTaxPaid = parcel.readDouble();
+
+    }
+
+    public static final Parcelable.Creator<CRACustomer> CREATOR = new Parcelable.Creator<CRACustomer>() {
+        @Override
+        public CRACustomer createFromParcel(Parcel parcel) {
+            return new CRACustomer(parcel);
+        }
+
+        @Override
+        public CRACustomer[] newArray(int size) {
+            return new CRACustomer[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(sinNumber);
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+        dest.writeString(fullName);
+        dest.writeString(personGender);
+        dest.writeDouble(fedTax);
+        dest.writeDouble(provTax);
+        dest.writeDouble(rrspCarryForward);
+        dest.writeDouble(grossIncome);
+        dest.writeDouble(rrspContribution);
+        dest.writeDouble(totalEI);
+        dest.writeDouble(totalTaxableAmount);
+        dest.writeDouble(totalTaxPaid);
+    }
+
+    public String amountFormatter() {
+        NumberFormat nf = NumberFormat.getInstance(new Locale("en", "US"));
+        String val = nf.format("$" + this);
+        return val;
     }
 }
--- app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/PersonInfoActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_midterm/PersonInfoActivity.java
@@ -3,8 +3,10 @@ package com.example.c0775696_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,58 +16,142 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class PersonInfoActivity extends AppCompatActivity {
 
-    private Button btnSubmit;
-    private Button btnClear;
+    private Button btnCalculate;
     private EditText edtFirstName;
     private EditText edtLastName;
     private EditText edtSinNumber;
-    private EditText edtSelectDate;
-    private TextInputLayout edtBirthDate;
+    private EditText edtDOB;
+    private TextView edtTaxFiledDate;
     private EditText edtGrossIncome;
     private EditText edtRRSP;
     private RadioButton rbtnMale;
     private RadioButton rbtnFemale;
     private RadioButton rbtnOther;
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-        clearAllField();
-    }
 
-    private void clearAllField() {
-
-            edtSinNumber.getText().clear();
-            edtFirstName.getText().clear();
-            edtLastName.getText().clear();
-            //edtSelectDate.getText().clear();
-           ///// edtGrossIncome.getText().clear();
-           // edtRRSP.getText().clear();
-           // rbtnMale.setChecked(false);
-           // rbtnFemale.setChecked(false);
-           // rbtnOther.setChecked(false);
-    }
+    private   DatePickerDialog picker;
+    final Calendar calendar = Calendar.getInstance();
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+    final   int month = calendar.get(Calendar.MONTH);
+    final int year = calendar.get(Calendar.YEAR);
+    String gender=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edtFirstName = findViewById(R.id.edtFirstNameText);
-        edtLastName = findViewById(R.id.edtLastNameText);
-        edtSinNumber = findViewById(R.id.edtSinnumber);
+        edtFirstName = findViewById(R.id.txtFirstName);
+        edtLastName = findViewById(R.id.txtLastName);
+        edtSinNumber = findViewById(R.id.txtSinNumber);
+        rbtnMale = findViewById(R.id.rdMale);
+        rbtnFemale = findViewById(R.id.rdFeMale);
+        rbtnOther = findViewById(R.id.rdOther);
+        edtGrossIncome = findViewById(R.id.txtGrossIncome);
+        edtRRSP =findViewById(R.id.txtRrsp);
+        edtDOB = findViewById(R.id.txtBirthDate);
+        edtTaxFiledDate = findViewById(R.id.txtTaxFillDate);
 
-        btnSubmit = findViewById(R.id.btnSubmit);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+        btnCalculate = findViewById(R.id.btnCalculate);
+        edtTaxFiledDate.setText(new StringBuilder()
+                .append(day).append(" ").append("-").append(month + 1).append("-")
+                .append(year));
+
+        edtDOB.setInputType(InputType.TYPE_NULL);
+        edtDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mIntent = new Intent(PersonInfoActivity.this, DataDisplayActivity.class);
-                startActivity(mIntent);
+
+                // date picker dialog
+                picker = new DatePickerDialog(PersonInfoActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                edtDOB.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fieldIsEmpty();
+                //Intent mIntent = new Intent(PersonInfoActivity.this, DataDisplayActivity.class);
+                //startActivity(mIntent);
             }
         });
 
     }
+
+    private void fieldIsEmpty() {
+        boolean Flag = false;
+        if(edtSinNumber.getText().toString().isEmpty())
+        {
+            edtSinNumber.setError("Please enter your SIN");
+            Flag = true;
+            return;
+        }
+        if(edtFirstName.getText().toString().isEmpty()){
+            edtFirstName.setError("Please enter your first name");
+            Flag = true;
+            return;
+        }
+        if(edtLastName.getText().toString().isEmpty())
+        {
+            edtLastName.setError("Please enter your date of birth");
+            Flag = true;
+            return;
+        }
+        if(edtDOB.getText().toString().isEmpty())
+        {
+            edtDOB.setError("Please enter your date of birth");
+            Flag = true;
+            return;
+        }
+        if(edtGrossIncome.getText().toString().isEmpty())
+        {
+            edtGrossIncome.setError("Please enter your Gross Income");
+            Flag = true;
+            return;
+        }
+        if(edtRRSP.getText().toString().isEmpty())
+        {
+            edtRRSP.setError("Please enter your date of birth");
+            Flag = true;
+            return;
+        }
+
+        if(!Flag)
+        {
+            CRACustomer craCustomer = new CRACustomer(edtSinNumber.getText().toString(),
+                    edtFirstName.getText().toString(),
+                    edtLastName.getText().toString(),
+                    edtDOB.getText().toString(),
+                    Double.parseDouble(edtGrossIncome.getText().toString()),
+                    Double.parseDouble(edtRRSP.getText().toString()));
+            Intent mIntent = new Intent(PersonInfoActivity.this, DataDisplayActivity.class);
+            mIntent.putExtra("CRACustomer", craCustomer);
+            startActivity(mIntent);
+        }
+    }
 }
+
 
